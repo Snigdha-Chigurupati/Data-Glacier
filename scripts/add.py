8c2175ba738c2b18fee23be514c4e3e2da8210d0
Snigdha-Chigurupati <114623884+Snigdha-Chigurupati@users.noreply.github.com>
2023-02-28 15:26:12 -0500

    Update add.py

--- scripts/add.py
+++ scripts/add.py
@@ -21,8 +21,8 @@ def write_json(data,filename = '../response.json'):
 
 
 def call_sport():
-    name = input("Please add your name: ")
-    sport = input("Please add your favourite sports name: ")
+    name = "Snigdha Chigurupati"
+    sport = "Football"
     if (sport == ""):
         sport = 'Cricket'
     if (name):
